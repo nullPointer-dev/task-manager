747d92c4cc4cd6368c4add62690ca36840143ea1
Sasanka Sekhar Panda <shashank35132@gmail.com>
2026-05-29 21:33:46 +0530

    added vercel origin to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app.include_router(router_users)
 app.include_router(router_tasks)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://task-manager-backend-cndl.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
